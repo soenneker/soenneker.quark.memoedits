f657573005e01ccd74e75dc4a192939388bd4806
Jake Soenneker <jake@soenneker.com>
2025-09-12 20:36:07 -0500

    Initial commit

---
+++ test/Soenneker.Quark.MemoEdits.Tests/MemoEditTests.cs
@@ -0,0 +1,18 @@
+﻿using Soenneker.Tests.FixturedUnit;
+using Xunit;
+
+namespace Soenneker.Quark.MemoEdits.Tests;
+
+[Collection("Collection")]
+public sealed class MemoEditTests : FixturedUnitTest
+{
+    public MemoEditTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    {
+    }
+
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
